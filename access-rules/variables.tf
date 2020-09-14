d6661d87f5de0bf7a268c16678d36f27649d091f
thanhbn87 <thanhbn87@gmail.com>
2020-09-14 10:59:21 +0700

    change module name to access-rules

---
+++ access-rules/variables.tf
@@ -0,0 +1,37 @@
+variable "cf_email" {
+  description = "The email associated with the account."
+  type        = string
+}
+
+variable "cf_api_key" {
+  description = "The Cloudflare API key."
+  type        = string
+}
+
+variable "cf_api_token" { 
+  description = "The Cloudflare API Token. This is an alternative to email+api_key. If both are specified, api_token will be used over email+api_key fields."
+  type        = string
+  default     = null
+}
+
+variable "account_id" {
+  description = "Configure API client with this account ID, so calls use the account API rather than the (default) user API."
+  type        = string
+  default     = null
+}
+
+variable "zone_id" {
+  description = "Configure API client with this account ID, so calls use the account API rather than the (default) user API."
+  type        = string
+  default     = null
+}
+
+variable "whitelist_ips" {
+  type    = list
+  default = []
+}
+
+variable "notes" {
+  type    = string
+  default = ""
+}
