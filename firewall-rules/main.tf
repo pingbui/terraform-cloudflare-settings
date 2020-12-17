bc0207cf74b36a38e3aec53d6687a6b0fafd0793
thanhbn87 <thanhbn87@gmail.com>
2020-12-17 16:46:52 +0700

    Add firewall_rule

--- firewall-rules/main.tf
+++ firewall-rules/main.tf
@@ -12,11 +12,20 @@ data "cloudflare_zones" "this" {
 }
 
 resource "cloudflare_filter" "this" {
-  for_each = { for rule in var.filewall_rules: rule.description => rule }
+  for_each    = { for rule in var.filewall_rules: rule.description => rule }
   
   description = lookup(each.value, "description")
   zone_id     = lookup(data.cloudflare_zones.this.zones[0], "id")
   paused      = lookup(each.value, "paused", false)
   expression  = lookup(each.value, "expression")
   ref         = lookup(each.value, "ref", null)
+}
+
+resource "cloudflare_firewall_rule" "this" {
+  for_each    = { for rule in var.filewall_rules: rule.description => rule }
+
+  zone_id     = lookup(data.cloudflare_zones.this.zones[0], "id")
+  description = lookup(each.value, "description")
+  filter_id   = [for ids in values(cloudflare_filter.this)[*]: ids.id if ids.description == lookup(each.value, "description")][0]
+  action      = lookup(each.value, "action", "block")
 }
--- firewall-rules/outputs.tf
+++ firewall-rules/outputs.tf
@@ -2,3 +2,8 @@ output "filter_ids" {
   description = "The Filter identifiers"
   value       = values(cloudflare_filter.this)[*]["id"]
 }
+
+output "firewall_rule_ids" {
+  description = "The firewall rule identifiers"
+  value       = values(cloudflare_firewall_rule.this)[*]["id"]
+}
